ce91a4284d85a548e0b51d7f1d7d4c4b3db9aee5
Matheus Caceres <matheus@formaweb.com.br>
2013-11-14 10:33:57 -0200

    Remove Config module after used

--- test/utils/utils_test.exs
+++ test/utils/utils_test.exs
@@ -1,6 +1,12 @@
 defmodule Weber.Utils.Test do
   use ExUnit.Case
 
+  teardown_all do
+    :code.delete(Config)
+    :code.purge(Config)
+    :ok
+  end
+
   test "get standard weber's config" do
     assert Weber.Utils.weber_config == Weber.DefaultConfig.config
   end
