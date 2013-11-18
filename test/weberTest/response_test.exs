bf85ca3a585c71411e29aef37747085f547aca6a
Matheus Caceres <matheus@formaweb.com.br>
2013-11-18 10:02:52 -0200

    Just initialize cowboy

--- test/weberTest/response_test.exs
+++ test/weberTest/response_test.exs
@@ -1,5 +1,17 @@
 defmodule WeberHttpResponseTest do
   use ExUnit.Case
+  import Weber.Test.Helpers
+
+  setup_all do
+    use_config([webserver: [session_manager: false]])
+    Cowboy.start(Weber.Utils.weber_config)
+    :ok
+  end
+
+  teardown_all do
+    purge_module(Config)
+    Cowboy.shutdown
+  end
 
   test "SimpleResponse test" do
     {:ok, status, _, client} = :hackney.request(:get, 'http://localhost:8080', [], <<>>, [])
